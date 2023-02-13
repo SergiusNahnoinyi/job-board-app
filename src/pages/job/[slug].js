9a813f9cfed620d4681c757397b15d7942b9fdb4
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-13 14:20:33 +0200

    Feature: implement getting props

--- src/pages/job/[slug].js
+++ src/pages/job/[slug].js
@@ -1,5 +1,24 @@
 import JobDetails from "@/components/JobDetails";
 
-export default function JobDetailsPage() {
+import { getJobs } from "@/services/contentful-service";
+
+export default function JobDetailsPage({ jobs }) {
+  console.log(jobs);
+
   return <JobDetails />;
 }
+
+export const getStaticProps = async () => {
+  try {
+    const jobs = await getJobs();
+    return {
+      props: {
+        jobs
+      }
+    };
+  } catch (error) {
+    return {
+      props: {}
+    };
+  }
+};
