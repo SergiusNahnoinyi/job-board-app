50c33ace5dd79446cbc40a7d7489d25d9464ab6d
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-13 14:51:04 +0200

    Feature: generate path

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,5 +1,15 @@
-const JobDetails = () => {
-  return <div>JobDetails</div>;
+import Link from "next/link";
+
+const JobDetails = ({ job }) => {
+  console.log(job);
+  return (
+    <>
+      <Link href="/" className="text-indigo-400">
+        Back To Jobs
+      </Link>
+      <div>{job.fields.title}</div>
+    </>
+  );
 };
 
 export default JobDetails;
--- src/pages/job/[slug].js
+++ src/pages/job/[slug].js
@@ -2,21 +2,42 @@ import JobDetails from "@/components/JobDetails";
 
 import { getJobs } from "@/services/contentful-service";
 
-export default function JobDetailsPage({ jobs }) {
-  console.log(jobs);
-
-  return <JobDetails />;
+export default function JobDetailsPage({ job }) {
+  return <JobDetails job={job} />;
 }
 
-export const getStaticProps = async () => {
+export const getStaticPaths = async () => {
   try {
     const jobs = await getJobs();
+    const slugs = jobs.map((job) => job.fields.slug);
+    const paths = slugs.map((slug) => ({ params: { slug } }));
+
+    return {
+      paths,
+      fallback: false
+    };
+  } catch (error) {
+    console.log(error);
+
+    return { paths: {}, fallback: false };
+  }
+};
+
+export const getStaticProps = async ({ params }) => {
+  const slug = params.slug;
+
+  try {
+    const jobs = await getJobs();
+    const job = jobs.filter((job) => job.fields.slug === slug)[0];
+
     return {
       props: {
-        jobs
+        job
       }
     };
   } catch (error) {
+    console.log(error);
+
     return {
       props: {}
     };
