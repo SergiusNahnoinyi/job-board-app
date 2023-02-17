05d718409fb5e84b1aa09f1501dc95bb6d971c53
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-17 12:33:31 +0200

    Feature: create heading

---
+++ src/components/Heading.jsx
@@ -0,0 +1,26 @@
+const Heading = ({ jobs }) => {
+  const title = `Found ${jobs.length} Jobs`;
+
+  switch (jobs.length) {
+    case 0: {
+      title = "No Jobs found.";
+      break;
+    }
+    case 1: {
+      title = "Only one Job found.";
+      break;
+    }
+    default:
+      break;
+  }
+
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-4">
+        <div className="text-sm text-slate-500 italic">{title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Heading;
